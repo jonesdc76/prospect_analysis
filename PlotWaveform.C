d6a435e4b94a44f50374e32ac348d477f071e5df
Donald Jones <jonesdc76@gmail.com>
2018-02-08 18:10:14 -0500

    Update to PlotWaveform.C

--- PlotWaveform.C
+++ PlotWaveform.C
@@ -30,7 +30,10 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
   
   Chain chain(series, first, last, dir, treename);
   chain.SetVerbose(1);
-  chain.CreateChain();
+  if(chain.GetNfiles()==0){
+    cout<<"No files added to chain. Exiting.\n";
+    return -1;
+  }
   TChain *ch  = chain.GetChain();
   ULong64_t trig;//event/trigger number propagated from the input raw file, for
                  //tracking back to the original source,
@@ -49,6 +52,7 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
   ch->SetBranchAddress("nsamp", &nsamp);
   ch->SetBranchAddress("samps", &samps);
   TGraph *gr;
+  TH1I *h = new TH1I("h","Number of Samples in Waveform",200,0,200);
   
   //create time/sample variable
   for(int i=0;i<kNmaxSamp;++i) {
@@ -67,10 +71,10 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
       mod += nent/10;
     }
     ch->GetEntry(i);
-    if((int)nsamp > nmax)nmax = (int)nsamp;
     
     if(single_pulse && chan == 124){
-      for(ULong64_t j=0;j<nsamp;++j)samples[j] = (int)samps[j];
+     if((int)nsamp > nmax)nmax = (int)nsamp;
+     for(ULong64_t j=0;j<nsamp;++j)samples[j] = (int)samps[j];
       gr = new TGraph(nsamp,t,samples);
       gr->SetTitle(Form("Channel %i",chan));
       gr->Draw("alp");
@@ -78,7 +82,10 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
       //sleep(1);
       cin>>sk;
       if(strcmp(sk,"q")==0)break;
-    }else if(chan > -1 && chan < 308){
+      //    }else if(chan > -1 && chan < 308){
+    }else if(chan>-1&&chan<308&&nsamp==60){
+     if((int)nsamp > nmax)nmax = (int)nsamp;
+     h->Fill(nsamp);
       for(ULong64_t j=0;j<nsamp;++j){
 	++n[j];
 	samples_D[j] = samples_D[j]/n[j]+ (n[j]-1)/n[j] * (double)samps[j];
@@ -86,6 +93,11 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
     }
   }
   cout<<endl;
+  if(single_pulse)return 0;
+  
+  TCanvas *c = new TCanvas("c","c",0,0,1400,600);
+  c->Divide(2,1);
+  c->cd(1);
   gr = new TGraph(nmax, t_D, samples_D);
   gr->SetMarkerStyle(22);
   gr->SetMarkerSize(0.6);
@@ -96,6 +108,8 @@ int PlotWaveform(Long64_t max_entries = 0, bool single_pulse = 0, int series = 1
   gr->GetXaxis()->SetTitle("Sample Number");
   gr->GetYaxis()->SetTitle("ADC units");
   gPad->Update();
+  c->cd(2);
+  h->Draw();
   time(&end);
   cout<<"Execution time: "<<difftime(end,start)<<" seconds for "<<nent<<" pulses"<<endl;
   return 0;
